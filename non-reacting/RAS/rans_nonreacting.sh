796d7548de0515bc0b3d43002e62de5cad04343a
Nick <nicholas.curtis@uconn.edu>
2018-04-18 15:58:13 -0400

    move to requeue

--- non-reacting/RAS/rans_nonreacting.sh
+++ non-reacting/RAS/rans_nonreacting.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#SBATCH --partition=debug         # run on parallel partition
-#SBATCH --time=00:30:00               # Job should run for no more than 6 hours
+#SBATCH --partition=general_requeue  # run on requeue partition
+#SBATCH --time=12:00:00              # Job should run for no more than 12 hours
 #SBATCH --nodes=1                    # one node
 #SBATCH --ntasks=24                  # 24 cores
 #SBATCH --exclude=cn[01-136,325-328] # only run on haswell
 #SBATCH --mem 100G                   # up to 100 gigs
 #SBATCH -o rans_nonreacting.out
 #SBATCH -e rans_nonreacting.err
-#SBATCH --mail-type=BEGIN,END,FAIL   # mail
+#SBATCH --mail-type=END              # mail
 #SBATCH --mail-user=nicholas.curtis@uconn.edu
 #SBATCH --dependency=singleton
 
