666f5a3de6eeae98c5d4f507bb54c481ce7743e0
Nick <nicholas.curtis@uconn.edu>
2018-04-20 11:10:30 -0400

    fix

--- plotting/axial_deficit_plots.py
+++ plotting/axial_deficit_plots.py
@@ -8,7 +8,7 @@ graph_name = 'axialDeficitPlot_{point}'
 
 
 def plot(case, reacting, t_start=0, t_end=-1):
-    for point in ['0point95', '1point53', '3point75', 'point375', '9point4']:
+    for point in ['0p95', '1p53', '03p75', '0p375', '9p4']:
         name = graph_name.format(point=point)
         expdata = read_experimental_data(name, args.reacting)
         simdata = read_simulation_data(case, name, reacting, t_start, t_end)
