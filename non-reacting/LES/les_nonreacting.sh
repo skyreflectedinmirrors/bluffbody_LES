855ebbf07aea5862d146c31dc4c26581fe166323
Nick <nicholas.curtis@uconn.edu>
2018-04-24 11:55:03 -0400

    fix time

--- non-reacting/LES/les_nonreacting.sh
+++ non-reacting/LES/les_nonreacting.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --partition=parallel         # run on parallel partition
-#SBATCH --time=12:00:00              # Job should run for no more than 12 hours
+#SBATCH --time=06:00:00              # Job should run for no more than 12 hours
 #SBATCH --nodes=2                    # two nodes
 #SBATCH --ntasks=48                  # 48 cores
 #SBATCH --exclude=cn[01-136,325-328] # only run on haswell
