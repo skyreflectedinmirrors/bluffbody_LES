027f928ace2a7673dafcc3a97b5c905a4dd4cbfb
Nick <nicholas.curtis@uconn.edu>
2018-04-24 11:47:03 -0400

    try start from scratch on 2-nodes

--- non-reacting/LES/decompose.sh
+++ non-reacting/LES/decompose.sh
@@ -1,4 +1,4 @@
 cp -r 0.orig 0
-mapFieldsPar ../RAS -fields "(U T p)" -sourceTime latestTime -consistent -mapMethod mapNearest
+# mapFieldsPar ../RAS -fields "(U T p)" -sourceTime latestTime -consistent -mapMethod mapNearest
 renumberMesh -latestTime -overwrite
 decomposePar
