08f955f4e76f3647bca23215a597bb1800c2585e
Nick <nicholas.curtis@uconn.edu>
2018-05-07 10:19:28 -0400

    fixesg

--- non-reacting/RAS_laminar/rans_nonreacting.sh
+++ non-reacting/RAS_laminar/rans_nonreacting.sh
@@ -12,4 +12,4 @@
 #SBATCH --dependency=singleton
 
 
-mpirun reactingFoam -case /home/njc07003/OpenFOAM/njc07003-5.x/run/volvo_flygmotor_AB/non-reacting/RAS -parallel
+mpirun reactingFoam -case . -parallel
