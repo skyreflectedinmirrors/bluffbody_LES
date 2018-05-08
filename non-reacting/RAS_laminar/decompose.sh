84cafb306003cd229ab9d68ddfb4b8f9ee1aa8f8
Nick <nicholas.curtis@uconn.edu>
2018-05-08 12:31:17 -0400

    fix decomposes

--- non-reacting/RAS_laminar/decompose.sh
+++ non-reacting/RAS_laminar/decompose.sh
@@ -1,1 +1,1 @@
-blockMesh && cp -r 0.orig 0 && mapFields ../potential && renumberMesh -latestTime -overwrite && decomposePar
+blockMesh && cp -rv 0.orig/* 0 && mapFields ../potential && renumberMesh -latestTime -overwrite && decomposePar
--- non-reacting/potential/decompose.sh
+++ non-reacting/potential/decompose.sh
@@ -1,1 +1,1 @@
-cp -r 0.orig/* 0
+blockMesh && checkMesh && cp -r 0.orig/* 0
