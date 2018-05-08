7352bb1577aeb1730cb0ac7e79e9663458a7d2ad
Nick <nicholas.curtis@uconn.edu>
2018-05-08 15:16:46 -0400

    fix decompose

--- non-reacting/RAS/decompose.sh
+++ non-reacting/RAS/decompose.sh
@@ -1,1 +1,1 @@
-cp -r 0.orig 0 && mapFields ../RAS_laminar -consistent && decomposePar && renumberMesh -latestTime -overwrite
+cp -rv 0.orig/* 0 && mapFields ../RAS_laminar -consistent && renumberMesh -latestTime -overwrite && decomposePar
