257871c4fc08216789123e668fdd22c76021dd91
Nick <nicholas.curtis@uconn.edu>
2018-05-08 15:23:17 -0400

    from parallel

--- non-reacting/RAS/decompose.sh
+++ non-reacting/RAS/decompose.sh
@@ -1,2 +1,2 @@
 rm -r 0
-mkdir -p 0 && cp -rv 0.orig/* 0 && mapFields ../RAS_laminar -consistent && renumberMesh -latestTime -overwrite && decomposePar
+mkdir -p 0 && cp -rv 0.orig/* 0 && mapFields ../RAS_laminar -consistent -sourceTime 'latestTime' -parallelSource && renumberMesh -latestTime -overwrite && decomposePar
