2f11152d3da973e98f6ec89d3f3ba339f57bc0e9
Nick <nicholas.curtis@uconn.edu>
2018-05-09 17:26:14 -0400

    update BC's

--- non-reacting/RAS_laminar/decompose.sh
+++ non-reacting/RAS_laminar/decompose.sh
@@ -1,1 +1,1 @@
-mkdir -p 0 && blockMesh && cp -rv 0.orig/* 0 && mapFields ../potential && renumberMesh -latestTime -overwrite && decomposePar
+mkdir -p 0 && cp -rv 0.orig/* 0 && mapFields ../potential && renumberMesh -latestTime -overwrite && decomposePar
