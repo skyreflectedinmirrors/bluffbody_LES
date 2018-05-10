db8aad9065258704c9dad2c4ebe29f8c90639d92
Nick <nicholas.curtis@uconn.edu>
2018-05-10 10:03:42 -0400

    have potential automatically convert to mass-flux

--- non-reacting/potential/potential.sh
+++ non-reacting/potential/potential.sh
@@ -1,1 +1,5 @@
 potentialFoam -writePhi -writep
+echo "Converting to mass flux"
+convertPhi -rhoRef `grep -r rho 0.orig/initialValues | sed -e 's/.*[^0-9]\([0-9]\+\)\.\([0-9]\+\)[^0-9]*$/\1\.\2/'`
+mv -v 0/rhop 0/p
+mv -v 0/rhophi 0/phi
