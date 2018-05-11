4175505803b557467aba938ee8cc7a670798d616
Nick <nicholas.curtis@uconn.edu>
2018-05-11 09:03:26 -0400

    try with mapped pressure field and fixed static pressure to avoid large pressure wave at onset of simulation

--- non-reacting/potential/potential.sh
+++ non-reacting/potential/potential.sh
@@ -2,6 +2,4 @@ potentialFoam -writePhi -writep
 echo "Converting to mass flux"
 convertPhi -rhoRef `grep -r rho 0.orig/initialValues | sed -e 's/.*[^0-9]\([0-9]\+\)\.\([0-9]\+\)[^0-9]*$/\1\.\2/'`
 mv -v 0/rhop 0/p
-# don't map pressure
-rm 0/p
 mv -v 0/rhophi 0/phi
