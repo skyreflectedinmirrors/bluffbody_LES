2b3a43b27ad7a37e250cd73e94b8ee5f3369a0b6
Nick <nicholas.curtis@uconn.edu>
2018-05-31 10:30:19 -0400

    fix

--- reacting/RAS/rans.sh
+++ reacting/RAS/rans.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --partition=parallel         # run on requeue partition
 #SBATCH --time=06:00:00              # Job should run for no more than 12 hours
-#SBATCH --nodes=1                    # one node
-#SBATCH --ntasks=24                  # 24 cores
+#SBATCH --nodes=2                    # one node
+#SBATCH --ntasks=48                  # 24 cores
 #SBATCH --exclude=cn[01-136,325-328] # only run on haswell
 #SBATCH --mem 100G                   # up to 100 gigs
 #SBATCH -o rans_reacting.out
