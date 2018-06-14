09850d8d09c76ab6300269bbcf09f3ef9eb12931
Nick <nicholas.curtis@uconn.edu>
2018-06-14 11:12:31 -0400

    update

--- reacting/RAS_twostep/decompose.sh
+++ reacting/RAS_twostep/decompose.sh
@@ -1,6 +1,6 @@
 rm -r 0
 mkdir -p 0 && cp -rv 0.orig/* 0
 blockMesh
-mapFields ../XiFoam -latestTime
+mapFields ../XiFoam -sourceTime 'latestTime' -parallelSource
 renumberMesh -latestTime -overwrite
 decomposePar
