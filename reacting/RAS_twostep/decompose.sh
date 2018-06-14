bb5bfe85330308dde23ad296f48d065793a9243e
Nick <nicholas.curtis@uconn.edu>
2018-06-14 12:15:03 -0400

    switch to non-reacting LES initialization & use sphere to cell

--- reacting/RAS_twostep/decompose.sh
+++ reacting/RAS_twostep/decompose.sh
@@ -1,6 +1,7 @@
 rm -r 0
 mkdir -p 0 && cp -rv 0.orig/* 0
 blockMesh
-mapFields ../XiFoam -sourceTime 'latestTime' -parallelSource
+mapFields ../../non-reacting/LES -sourceTime 'latestTime' -parallelSource
+setFields
 renumberMesh -latestTime -overwrite
 decomposePar
