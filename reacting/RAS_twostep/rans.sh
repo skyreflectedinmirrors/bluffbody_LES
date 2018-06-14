762e2ae76765a758359da7d864092d37c0cbe679
Nick <nicholas.curtis@uconn.edu>
2018-06-14 10:55:34 -0400

    add two-step RAS

---
+++ reacting/RAS_twostep/rans.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --partition=parallel         # run on requeue partition
+#SBATCH --time=06:00:00              # Job should run for no more than 12 hours
+#SBATCH --nodes=2                    # one node
+#SBATCH --ntasks=48                  # 24 cores
+#SBATCH --exclude=cn[01-136,325-328] # only run on haswell
+#SBATCH --mem 100G                   # up to 100 gigs
+#SBATCH -o rans_reacting.out
+#SBATCH -e rans_reacting.err
+#SBATCH --mail-type=END              # mail
+#SBATCH --mail-user=nicholas.curtis@uconn.edu
+#SBATCH --dependency=singleton
+#SBATCH --signal=SIGTERM@60
+
+
+mpirun reactingFoam -case . -parallel
