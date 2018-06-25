97840a50d35f819bf14aaa2c7a48f40f9281ef5a
Nick <nicholas.curtis@uconn.edu>
2018-06-25 09:05:37 -0400

    have RAS use mapping from two-step

--- reacting/RAS/decompose.sh
+++ reacting/RAS/decompose.sh
@@ -1,7 +1,7 @@
 rm -r 0
 mkdir -p 0 && cp -rv 0.orig/* 0
 blockMesh
-mapFields ../potential
+mapFields ../RAS_twostep -sourceTime latestTime -consistent
 setFields
 renumberMesh -latestTime -overwrite
 decomposePar
