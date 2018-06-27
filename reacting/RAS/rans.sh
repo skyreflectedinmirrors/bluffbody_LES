7a28816bf70c5373ffe0a799406f2dca100353da
Nick <nicholas.curtis@uconn.edu>
2018-06-27 16:53:29 -0400

    switch over to IPM-instrumented solver

--- reacting/RAS/rans.sh
+++ reacting/RAS/rans.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --partition=parallel         # run on requeue partition
-#SBATCH --time=06:00:00              # Job should run for no more than 12 hours
+#SBATCH --partition=serial           # run on requeue partition
+#SBATCH --time=7-00:00:00            # Job should run for no more than 12 hours
 #SBATCH --nodes=2                    # one node
 #SBATCH --ntasks=48                  # 24 cores
 #SBATCH --exclude=cn[01-136,325-328] # only run on haswell
@@ -10,7 +10,8 @@
 #SBATCH --mail-type=END              # mail
 #SBATCH --mail-user=nicholas.curtis@uconn.edu
 #SBATCH --dependency=singleton
-#SBATCH --signal=SIGTERM@60
+#SBATCH --signal=USR1@60
 
-
-mpirun reactingFoam -case . -parallel
+export IPM_NESTED_REGIONS=1
+export JOB="reactingFoamIPM -parallel"
+srun -N$SLURM_JOB_NUM_NODES -n$SLURM_NTASKS --mpi=openmpi $JOB
