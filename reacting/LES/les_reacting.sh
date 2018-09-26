e18b531c496e57063fd55c01f348a5213f82ed41
Nick <nicholas.curtis@uconn.edu>
2018-09-26 16:30:33 -0400

    update

--- reacting/LES/les_reacting.sh
+++ reacting/LES/les_reacting.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --partition=parallel	     # run on parallel partition
 #SBATCH --time=6:00:00               # Job should run for no more than 12 hours
-#SBATCH --nodes=2                    # two nodes
-#SBATCH --ntasks=48                  # 48 cores
+#SBATCH --nodes=4                    # two nodes
+#SBATCH --ntasks=96                  # 48 cores
 #SBATCH --exclude=cn[01-136,325-328] # only run on haswell
 #SBATCH --mem 100G                   # up to 100 gigs
 #SBATCH -o les_reacting.out
