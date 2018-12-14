de2f0ae940523e8245cd945d6553a18944a99653
Nick <nicholas.curtis@uconn.edu>
2018-12-14 11:29:45 -0500

    fix decompose source

--- reacting/LES/decompose.sh
+++ reacting/LES/decompose.sh
@@ -1,6 +1,6 @@
 rm -r 0
 mkdir -p 0 && cp -rv 0.orig/* 0
 blockMesh
-mapFields ../RAS -sourceTime latestTime -consistent
+mapFields ../LES_accelerint_coarse -sourceTime latestTime -consistent -parallelSource
 renumberMesh -latestTime -overwrite
 decomposePar
