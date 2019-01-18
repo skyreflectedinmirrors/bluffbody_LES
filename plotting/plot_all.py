6e59a34918745db827c233a059ec779db482673c
Nick <nicholas.curtis@uconn.edu>
2019-01-18 15:15:21 -0500

    add reynolds stress to plotall

--- plotting/plot_all.py
+++ plotting/plot_all.py
@@ -3,8 +3,9 @@ from common import get_default_parsing_args, UserOptions
 from fluctuation_velocity_plots import plot as fplot
 from mean_axial_velocity import plot as mplot
 from axial_deficit_plots import plot as aplot
+from reynolds_stress_plots import plot as rplot
 
-plotters = [fplot, mplot, aplot]
+plotters = [fplot, mplot, aplot, rplot]
 
 if __name__ == '__main__':
     parser = get_default_parsing_args(
