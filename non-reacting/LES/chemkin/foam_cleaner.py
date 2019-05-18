9a67406dd75f65ba20d5603a6e5a24f4fb838d00
Nick <nicholas.curtis@uconn.edu>
2019-05-18 14:04:22 -0400

    add cleaned up galway thermo file for chemkinToFoam

---
+++ non-reacting/LES/chemkin/foam_cleaner.py
@@ -0,0 +1,26 @@
+import re
+import cantera as ct
+
+# comment out all species that exist in the thermo database that aren't in the model
+reglist = [re.compile('^[ -]\\d\\.\\d+E[+-]\\d+'), re.compile('^\\s*!'),
+           re.compile(r'^\s*\n\s*$'), re.compile(r'^( )+'),
+           re.compile(r'^\s*THERMO'), re.compile(r'^\s*END')]
+
+gas = ct.Solution('galway.cti')
+
+with open('56.54_therm.dat.txt') as file:
+    lines = file.readlines()
+
+do_comment = 0
+for i, line in enumerate(lines):
+    if not any(r.search(line) for r in reglist) and line.strip():
+        name = line[:line.index(' ')]
+        if name not in gas.species_names:
+            assert not do_comment
+            do_comment = 4
+    if do_comment:
+        lines[i] = '!' + lines[i]
+        do_comment -= 1
+
+with open('56.54_therm_clean.dat.txt', 'w') as file:
+    file.writelines(lines)
