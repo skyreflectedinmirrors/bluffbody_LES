d99af71f497b4083a626fe882a62eee000b9bd63
Nick <nicholas.curtis@uconn.edu>
2019-05-24 08:52:16 -0400

    update haswell nodes

--- reacting/LES_accelerint/les_reacting.sh
+++ reacting/LES_accelerint/les_reacting.sh
@@ -3,7 +3,7 @@
 #SBATCH --time=6:00:00               # Job should run for no more than 12 hours
 #SBATCH --nodes=4                    # two nodes
 #SBATCH --ntasks=96                  # 48 cores
-#SBATCH --exclude=cn[01-136,325-328] # only run on haswell
+#SBATCH --exclude=cn[65-69,71-136,325-343,345-353,355-358,360-364,369-398,400-401],gpu[07-10] # only run on haswell
 #SBATCH --mem 100G                   # up to 100 gigs
 #SBATCH -o les_reacting.out
 #SBATCH -e les_reacting.out
--- reacting/RAS/rans.sh
+++ reacting/RAS/rans.sh
@@ -2,7 +2,7 @@
 #SBATCH --partition=serial           # run on serial partition
 #SBATCH --nodes=1                    # one node
 #SBATCH --ntasks=24                  # 24 cores
-#SBATCH --exclude=cn[01-136,325-328] # only run on haswell
+#SBATCH --exclude=cn[65-69,71-136,325-343,345-353,355-358,360-364,369-398,400-401],gpu[07-10] # only run on haswell
 #SBATCH --mem 100G                   # up to 100 gigs
 #SBATCH -e rans_reacting.err
 #SBATCH --mail-type=END              # mail
