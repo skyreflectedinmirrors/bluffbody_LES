bb098c33ee0b718371937e2ff200e0fcd4c5947d
Nick <nicholas.curtis@uconn.edu>
2019-06-15 12:23:25 -0400

    fix decompose for LES

--- bluffbody/reacting/LES_coarse/galway/decompose.sh
+++ bluffbody/reacting/LES_coarse/galway/decompose.sh
@@ -1,6 +1,6 @@
 rm -r 0
 mkdir -p 0 && cp -rv 0.orig/* 0
 blockMesh
-mapFields ../../LES_accelerint_coarse -sourceTime latestTime -consistent
+mapFields ../../LES_accelerint_coarse/galway -sourceTime $1 -consistent
 renumberMesh -latestTime -overwrite
 decomposePar
